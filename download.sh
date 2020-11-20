31fd6d47e514c062fa0b0c5df70767748d33dc45
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2020-11-20 21:28:53 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,8 @@
-
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/cup.php	-output /var/www/html/admin/cup.php
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/index.html	-output /var/www/html/admin/index.html
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/cup.php	-output /var/www/html/admin/cup.php
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/meek.css	-output /var/www/html/admin/meek.css
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/nodered.png	-output /var/www/html/admin/nodered.png
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/password	-output /var/www/html/admin/password
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/ubuntu.png	-output /var/www/html/admin/ubuntu.png
+curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/zigbee2mqtt.png	-output /var/www/html/admin/zigbee2mqtt.png
