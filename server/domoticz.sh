5648312d8e9dddfe95b89dcea9609d7ad602e20f
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-01 20:25:23 +0200

    Update domoticz.sh

--- server/domoticz.sh
+++ server/domoticz.sh
@@ -2,6 +2,7 @@
 
 # 1) Create container in unpriviliged mode
 # 2) Enable container -nesting- & -NFS-
+# 3) apt-get install curl -y && curl https://raw.githubusercontent.com/Meek-HA/Meek-io/server/master/domoticz.sh --output domoticz.sh && chmod +rwx domoticz.sh && ./domoticz.sh
 
 echo Update System
 apt-get update -y
