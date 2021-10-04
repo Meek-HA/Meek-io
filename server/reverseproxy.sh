314109af2e64124803a38d239fc49c6a9556eb53
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-04 17:37:22 +0200

    Update reverseproxy.sh

--- server/reverseproxy.sh
+++ server/reverseproxy.sh
@@ -2,6 +2,7 @@
 
 # 1) Create container "reverseproxy" in unpriviliged mode
 # 2) Enable container -nesting- & -NFS-
+# 3) apt-get install curl -y && curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/server/reverseproxy.sh --output reverseproxy.sh && chmod +rwx reverseproxy.sh && ./reverseproxy.sh
 
 echo Update System
 apt-get update -y
