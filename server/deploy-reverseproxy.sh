91d721ffad06c5d43b9046f6443d031534663b82
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-27 11:59:23 +0200

    Update deploy-reverseproxy.sh

--- server/deploy-reverseproxy.sh
+++ server/deploy-reverseproxy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/server/deploy-reverseproxy.sh --output deploy-reverseproxy.sh && chmod +rwx deploy-reverseproxy.sh && ./deploy-reverseproxy.sh
+# xxxxxx = subdomain
+# yyyyyy = domain
+
 
 ######--Set Sub-Domain--################################################
 echo -n "Enter subdomain name : "
@@ -24,12 +27,12 @@ cat <<'EOF'> /etc/nginx/sites-enabled/$NAME.$opt.conf
 # xxxxxx = subdomain
 server {
 listen 80;
-server_name xxxxxx.$opt;
+server_name xxxxxx.yyyyyy;
 return 301 https://$host$request_uri;
 }
 server {
 listen       443 ssl http2;
-server_name xxxxxx.$opt;
+server_name xxxxxx.yyyyyy;
 #Domoticz server in de Root
 location / {
 proxy_set_header Upgrade $http_upgrade;
@@ -84,7 +87,7 @@ proxy_redirect off;
 #Homebridge op specifik port in root
 server {
 listen 8581 ssl http2;
-server_name xxxxxx.$opt;
+server_name xxxxxx.yyyyyy;
 location / {
 proxy_pass                  http://xxxxxx:8581;
 proxy_http_version          1.1;
@@ -95,13 +98,13 @@ proxy_set_header            Connection "Upgrade";
 proxy_set_header            X-Real-IP $remote_addr;
 proxy_set_header            X-Forward-For $proxy_add_x_forwarded_for;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx.$opt/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx.$opt/privkey.pem; # managed by Certbot
+ssl_certificate /etc/letsencrypt/live/xxxxxx.yyyyyy/fullchain.pem; # managed by Certbot
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx.yyyyyy/privkey.pem; # managed by Certbot
 }
 #proxy for node-red @ port :1880
 server {
 listen 1880 ssl http2;
-server_name xxxxxx.$opt;
+server_name xxxxxx.yyyyyy;
 location = /robots.txt {
 add_header  Content-Type  text/plain;
 return 200 "User-agent: *\nDisallow: /\n";
@@ -119,12 +122,13 @@ proxy_set_header X-Forwarded-Proto $scheme;
 proxy_set_header X-Forwarded-Host $host;
 proxy_set_header X-Forwarded-Port $server_port;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx.$opt/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx.$opt/privkey.pem; # managed by Certbot
+ssl_certificate /etc/letsencrypt/live/xxxxxx.yyyyyy/fullchain.pem; # managed by Certbot
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx.yyyyyy/privkey.pem; # managed by Certbot
 }
 EOF
 
 sed -i -e "s/xxxxxx/$NAME/g" /etc/nginx/sites-enabled/$NAME.$opt.conf
+sed -i -e "s/yyyyyy/$opt/g" /etc/nginx/sites-enabled/$NAME.$opt.conf
 
 ######--Issue new Certificte--################################################
 certbot --nginx -d $NAME.$opt
