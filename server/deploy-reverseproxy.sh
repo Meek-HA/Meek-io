2e2cefdd95cf71901c01e40da2c41c9087c26e16
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-27 12:45:36 +0200

    Update deploy-reverseproxy.sh

--- server/deploy-reverseproxy.sh
+++ server/deploy-reverseproxy.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 
 # curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/server/deploy-reverseproxy.sh --output deploy-reverseproxy.sh && chmod +rwx deploy-reverseproxy.sh && ./deploy-reverseproxy.sh
-# xxxxxx = subdomain
-# yyyyyy = domain
+# xxxxxx = Full domainname
 # zzzzzz = IP container
 
-######--Get IP from new container--################################################
+######--Get IP & full domainname for new container--################################################
 IP="$(head -1 /mnt/certificate/deploy/user)"
-
-######--Set Sub-Domain--################################################
-echo -n "Enter subdomain name : "
-read NAME
-
-######--Set Domain--################################################
-echo Select Domain Name
-prompt="Select Domain:"
-options=("meek-io.com" "mymeek.org")
-PS3="$prompt "
-select opt in "${options[@]}" "Quit"; do
-    case "$REPLY" in
-    1) echo "You picked $opt which is option 1"; domn=$opt; break;;
-    2) echo "You picked $opt which is option 2"; domn=$opt; break;;
-    $((${#options[@]}+1))) echo "TOP!"; break;;
-    *) echo "Invalid input. Try again!";continue;;
-    esac
-done
+NAME="$(tail -1 /mnt/certificate/deploy/user)"
 
 cat <<'EOF'> /etc/nginx/sites-enabled/$NAME.$opt.conf
 # xxxxxx = subdomain
 server {
 listen 80;
-server_name xxxxxx.yyyyyy;
+server_name xxxxxx;
 return 301 https://$host$request_uri;
 }
 server {
 listen       443 ssl http2;
-server_name xxxxxx.yyyyyy;
+server_name xxxxxx;
 #Domoticz server in de Root
 location / {
 proxy_set_header Upgrade $http_upgrade;
@@ -90,7 +72,7 @@ proxy_redirect off;
 #Homebridge op specifik port in root
 server {
 listen 8581 ssl http2;
-server_name xxxxxx.yyyyyy;
+server_name xxxxxx;
 location / {
 proxy_pass                  http://zzzzzz:8581;
 proxy_http_version          1.1;
@@ -101,13 +83,13 @@ proxy_set_header            Connection "Upgrade";
 proxy_set_header            X-Real-IP $remote_addr;
 proxy_set_header            X-Forward-For $proxy_add_x_forwarded_for;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx.yyyyyy/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx.yyyyyy/privkey.pem; # managed by Certbot
+ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem; # managed by Certbot
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem; # managed by Certbot
 }
 #proxy for node-red @ port :1880
 server {
 listen 1880 ssl http2;
-server_name xxxxxx.yyyyyy;
+server_name xxxxxx;
 location = /robots.txt {
 add_header  Content-Type  text/plain;
 return 200 "User-agent: *\nDisallow: /\n";
@@ -125,14 +107,13 @@ proxy_set_header X-Forwarded-Proto $scheme;
 proxy_set_header X-Forwarded-Host $host;
 proxy_set_header X-Forwarded-Port $server_port;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx.yyyyyy/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx.yyyyyy/privkey.pem; # managed by Certbot
+ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem; # managed by Certbot
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem; # managed by Certbot
 }
 EOF
 
-sed -i -e "s/xxxxxx/$NAME/g" /etc/nginx/sites-enabled/$NAME.$opt.conf
-sed -i -e "s/yyyyyy/$opt/g" /etc/nginx/sites-enabled/$NAME.$opt.conf
-sed -i -e "s/zzzzzz/$IP/g" /etc/nginx/sites-enabled/$NAME.$opt.conf
+sed -i -e "s/xxxxxx/$NAME/g" /etc/nginx/sites-enabled/$NAME.conf
+sed -i -e "s/zzzzzz/$IP/g" /etc/nginx/sites-enabled/$NAME.conf
 
 ######--Issue new Certificte--################################################
 certbot --nginx -d $NAME.$opt
