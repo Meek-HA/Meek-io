cd6d6306432e9fa9b54859bfb04293cdd622df68
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-27 12:26:18 +0200

    Update master-reverseproxy.sh

--- server/master-reverseproxy.sh
+++ server/master-reverseproxy.sh
@@ -13,7 +13,7 @@ timedatectl set-timezone Europe/Amsterdam
 
 echo NFS Share
 apt install nfs-kernel-server -y
-mkdir -p /mnt/certificate
+mkdir -p /mnt/certificate/deploy/user
 chown -R nobody:nogroup /mnt/certificate/
 chmod 777 /mnt/certificate/
 cat << EOF > /etc/exports
