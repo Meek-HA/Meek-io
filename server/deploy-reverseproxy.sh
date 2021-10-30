aa2f574ea8311c5877cd3fa6a6fff1509a2d23c3
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2021-10-30 10:06:04 +0200

    Update deploy-reverseproxy.sh

--- server/deploy-reverseproxy.sh
+++ server/deploy-reverseproxy.sh
@@ -9,16 +9,9 @@ IP="$(head -1 /etc/letsencrypt/live/deploy/user)"
 NAME="$(tail -1 /etc/letsencrypt/live/deploy/user)"
 
 cat <<'EOF'> /root/new.conf
-# xxxxxx = Full domain
 server {
-listen 80;
 server_name xxxxxx;
-return 301 https://$host$request_uri;
-}
-server {
-listen       443 ssl http2;
-server_name xxxxxx;
-#Domoticz server in de Root
+#Domoticz server in Root
 location / {
 proxy_set_header Upgrade $http_upgrade;
 proxy_set_header Connection "upgrade";
@@ -40,7 +33,7 @@ return 301 https://xxxxxx:1880;
 location /homebridge {
 return 301 https://xxxxxx:8581;
 }
-#Dashticz subpath forwardoing
+#Dashticz subpath forwarding
 location /dashticz {
 proxy_pass_header Authorization;
 proxy_pass http://zzzzzz:81/dashticz;
@@ -54,7 +47,7 @@ client_max_body_size 0;
 proxy_read_timeout 36000s;
 proxy_redirect off;
 }
-#Dashticz subpath forwardoing
+#Dashticz subpath forwarding
 location /admin {
 proxy_pass_header Authorization;
 proxy_pass http://zzzzzz:82/admin;
@@ -69,26 +62,83 @@ proxy_read_timeout 36000s;
 proxy_redirect off;
 }
 }
-#Homebridge op specifik port in root
+EOF
+
+sed -i -e "s/xxxxxx/$NAME/g" /root/new.conf
+sed -i -e "s/zzzzzz/$IP/g" /root/new.conf
+mv /root/new.conf /etc/nginx/sites-enabled/$NAME.conf
+rm /etc/letsencrypt/live/deploy/user
+
+######--Issue new Certificte--################################################
+certbot --nginx -d $NAME.$opt
+
+rm /root/new.conf
+rm /root/new.conf /etc/nginx/sites-enabled/$NAME.conf
+
+cat <<'EOF'> /root/new.conf
 server {
-listen 8581 ssl http2;
 server_name xxxxxx;
+#Domoticz server in Root
 location / {
-proxy_pass                  http://zzzzzz:8581;
-proxy_http_version          1.1;
-proxy_buffering             off;
-proxy_set_header            Host $host;
-proxy_set_header            Upgrade $http_upgrade;
-proxy_set_header            Connection "Upgrade";
-proxy_set_header            X-Real-IP $remote_addr;
-proxy_set_header            X-Forward-For $proxy_add_x_forwarded_for;
+proxy_set_header Upgrade $http_upgrade;
+proxy_set_header Connection "upgrade";
+proxy_pass_header Authorization;
+proxy_pass http://zzzzzz:81;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_http_version 1.1;
+proxy_set_header Connection "";
+proxy_buffering off;
+client_max_body_size 0;
+proxy_read_timeout 36000s;
+proxy_redirect off;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem; # managed by Certbot
+location /nodered {
+return 301 https://xxxxxx:1880;
 }
-#proxy for node-red @ port :1880
+location /homebridge {
+return 301 https://xxxxxx:8581;
+}
+#Dashticz subpath forwarding
+location /dashticz {
+proxy_pass_header Authorization;
+proxy_pass http://zzzzzz:81/dashticz;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_http_version 1.1;
+proxy_set_header Connection "";
+proxy_buffering off;
+client_max_body_size 0;
+proxy_read_timeout 36000s;
+proxy_redirect off;
+}
+#Dashticz subpath forwarding
+location /admin {
+proxy_pass_header Authorization;
+proxy_pass http://zzzzzz:82/admin;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_http_version 1.1;
+proxy_set_header Connection "";
+proxy_buffering off;
+client_max_body_size 0;
+proxy_read_timeout 36000s;
+proxy_redirect off;
+}
+listen 443 ssl;
+ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
+include /etc/letsencrypt/options-ssl-nginx.conf;
+ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+}
+
+#Node-Red port :1880
 server {
-listen 1880 ssl http2;
+listen [::]:1880;
+
 server_name xxxxxx;
 location = /robots.txt {
 add_header  Content-Type  text/plain;
@@ -107,17 +157,47 @@ proxy_set_header X-Forwarded-Proto $scheme;
 proxy_set_header X-Forwarded-Host $host;
 proxy_set_header X-Forwarded-Port $server_port;
 }
-ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem; # managed by Certbot
-ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem; # managed by Certbot
+listen 1880 ssl;
+ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
+include /etc/letsencrypt/options-ssl-nginx.conf;
+ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+}
+
+#Homebridge port 8581
+server {
+listen [::]:8581;
+server_name xxxxxx;
+location / {
+proxy_pass                  http://zzzzzz:8581;
+proxy_http_version          1.1;
+proxy_buffering             off;
+proxy_set_header            Host $host;
+proxy_set_header            Upgrade $http_upgrade;
+proxy_set_header            Connection "Upgrade";
+proxy_set_header            X-Real-IP $remote_addr;
+proxy_set_header            X-Forward-For $proxy_add_x_forwarded_for;
+}
+listen 8581 ssl;
+ssl_certificate /etc/letsencrypt/live/xxxxxx/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
+include /etc/letsencrypt/options-ssl-nginx.conf;
+ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+}
+
+server {
+if ($host = xxxxxx) {
+return 301 https://$host$request_uri;
+}
+
+server_name xxxxxx;
+listen 80;
+return 404;
 }
 EOF
 
 sed -i -e "s/xxxxxx/$NAME/g" /root/new.conf
 sed -i -e "s/zzzzzz/$IP/g" /root/new.conf
 mv /root/new.conf /etc/nginx/sites-enabled/$NAME.conf
-rm /etc/letsencrypt/live/deploy/user
-
-######--Issue new Certificte--################################################
-certbot --nginx -d $NAME.$opt
 
 service nginx reload
