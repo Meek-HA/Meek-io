7b1bbb22bd5c72785f684f024171353edb5bf577
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2022-01-29 08:59:06 +0100

    Update master-domoticz.sh

--- server/master-domoticz.sh
+++ server/master-domoticz.sh
@@ -4,9 +4,9 @@
 # 2) Enable container -nesting- & -NFS-
 # 3) apt-get install curl -y && curl https://raw.githubusercontent.com/Meek-HA/Meek-io/master/server/master-domoticz.sh --output domoticz.sh && chmod +rwx domoticz.sh && ./domoticz.sh
 
-echo Update System
-apt-get update -y
-apt-get upgrade -y
+#echo Update System
+#apt-get update -y
+#apt-get upgrade -y
 
 echo Set TimZone to Europe/Amsterdam
 timedatectl set-timezone Europe/Amsterdam
