3d37fe5315bc0d37d68e2cb1a6510b67399f8062
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2022-02-16 12:27:38 +0100

    Update deploy-reverseproxy.sh

--- server/deploy-reverseproxy.sh
+++ server/deploy-reverseproxy.sh
@@ -5,8 +5,9 @@
 # zzzzzz = IP container
 
 ######--Get IP & full domainname for new container--################################################
-IP="$(head -1 /etc/letsencrypt/live/deploy/user)"
-NAME="$(tail -1 /etc/letsencrypt/live/deploy/user)"
+cp /etc/letsencrypt/live/deploy/user /root/user
+IP="$(head -1 /root/user)"
+NAME="$(tail -1 /root/user)"
 
 cat <<'EOF'> /root/new.conf
 server {
@@ -27,53 +28,10 @@ client_max_body_size 0;
 proxy_read_timeout 36000s;
 proxy_redirect off;
 }
-location /nodered {
-return 301 https://zzzzzz:1880;
-}
-location /homebridge {
-return 301 https://zzzzzz:8581;
-}
-#Dashticz subpath forwarding
-location /dashticz {
-proxy_pass_header Authorization;
-proxy_pass http://zzzzzz:81/dashticz;
-proxy_set_header Host $host;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_http_version 1.1;
-proxy_set_header Connection "";
-proxy_buffering off;
-client_max_body_size 0;
-proxy_read_timeout 36000s;
-proxy_redirect off;
-}
-#Dashticz subpath forwarding
-location /admin {
-proxy_pass_header Authorization;
-proxy_pass http://zzzzzz:82/admin;
-proxy_set_header Host $host;
-proxy_set_header X-Real-IP $remote_addr;
-proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-proxy_http_version 1.1;
-proxy_set_header Connection "";
-proxy_buffering off;
-client_max_body_size 0;
-proxy_read_timeout 36000s;
-proxy_redirect off;
-}
 }
 EOF
 
-sed -i -e "s/xxxxxx/$NAME/g" /root/new.conf
-sed -i -e "s/zzzzzz/$IP/g" /root/new.conf
-mv /root/new.conf /etc/nginx/sites-enabled/$NAME.conf
-
-######--Issue new Certificte--################################################
-certbot --nginx -d $NAME.$opt
-
-rm /root/new.conf
-
-cat <<'EOF'> /root/new.conf
+cat <<'EOF'> /root/newssl.conf
 server {
 server_name xxxxxx;
 #Domoticz server in Root
@@ -132,11 +90,9 @@ ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
 include /etc/letsencrypt/options-ssl-nginx.conf;
 ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 }
-
 #Node-Red port :1880
 server {
 listen [::]:1880;
-
 server_name xxxxxx;
 location = /robots.txt {
 add_header  Content-Type  text/plain;
@@ -161,7 +117,6 @@ ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
 include /etc/letsencrypt/options-ssl-nginx.conf;
 ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 }
-
 #Homebridge port 8581
 server {
 listen [::]:8581;
@@ -182,12 +137,10 @@ ssl_certificate_key /etc/letsencrypt/live/xxxxxx/privkey.pem;
 include /etc/letsencrypt/options-ssl-nginx.conf;
 ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 }
-
 server {
 if ($host = xxxxxx) {
 return 301 https://$host$request_uri;
 }
-
 server_name xxxxxx;
 listen 80;
 return 404;
@@ -197,7 +150,28 @@ EOF
 sed -i -e "s/xxxxxx/$NAME/g" /root/new.conf
 sed -i -e "s/zzzzzz/$IP/g" /root/new.conf
 mv /root/new.conf /etc/nginx/sites-enabled/$NAME.conf
+sed -i -e "s/xxxxxx/$NAME/g" /root/newssl.conf
+sed -i -e "s/zzzzzz/$IP/g" /root/newssl.conf
 
-service nginx reload
+######--Issue new Certificte--################################################
+cat <<'EOF'> /root/cert.sh
+#/bin/sh
+echo -n "If you proceed, you will deploy domain xxxxxx with container IP zzzzzz ! (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+        certbot run -n --nginx --agree-tos -d xxxxxx -m  cert@mymeek.org --redirect
+        mv /root/newssl.conf /etc/nginx/sites-enabled/xxxxxx.conf
+        service nginx reload
+        rm /etc/letsencrypt/live/deploy/user
+        echo Done, browse to xxxxxx to check
+else
+    echo No
+fi
+EOF
 
-rm /etc/letsencrypt/live/deploy/user
+sed -i -e "s/xxxxxx/$NAME/g" /root/cert.sh
+sed -i -e "s/zzzzzz/$IP/g" /root/cert.sh
+chmod +rwx /root/cert.sh
