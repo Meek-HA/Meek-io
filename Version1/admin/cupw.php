ed7992632b74f89bb44b13ea9402324bf353063d
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2022-12-05 16:59:23 +0100

    Create cupw.php

---
+++ Version1/admin/cupw.php
@@ -0,0 +1,12 @@
+<?php
+        $Name = "Username:".$_POST['username']."";
+        $Pass = "Password:".$_POST['pwd2']."";
+        $data = $_POST['username']. PHP_EOL .$_POST['pwd2']. PHP_EOL;
+        $file=fopen("command/cup", "w");
+        fwrite($file, $data);
+        fclose($file);
+        echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
+        if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+            }
+?>
