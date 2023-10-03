72fbc44c1dcdbf648f1d4915b532090ea4832b36
Meek Home Automation <44222882+Meek-HA@users.noreply.github.com>
2023-10-03 22:17:53 +0200

    Update deploy-reverseproxy.sh

--- Version1/deploy-reverseproxy.sh
+++ Version1/deploy-reverseproxy.sh
@@ -39,7 +39,7 @@ location / {
 proxy_set_header Upgrade $http_upgrade;
 proxy_set_header Connection "upgrade";
 proxy_pass_header Authorization;
-proxy_pass http://zzzzzz:81;
+proxy_pass http://zzzzzz:8080;
 proxy_set_header Host $host;
 proxy_set_header X-Real-IP $remote_addr;
 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
